e18b8c858a6b3175f318615ff995afde8ebbe775
Nakharin Suwannapoom <101672708+Xigasive@users.noreply.github.com>
2024-07-14 10:03:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ const server =  http.createServer(app)
 const API_PORT = process.env
 const PORT = process.env.API_PORT
 
+fetch('/api/login', {  
+  method: 'POST',  
+  body: JSON.stringify({ email, password }),  
+  headers: {  
+    'Content-Type': 'application/json',  
+  },  
+})  
+.then(response => response.json())  
+.then(data => {  
+  // รับ Token จาก response  
+  const token = data.token;  
 
+  // เก็บ Token ใน localStorage  
+  localStorage.setItem('token', token);  
+})  
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
