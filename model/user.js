5156309410e751a8072fd90cc9073621b968e870
Nakharin Suwannapoom <101672708+Xigasive@users.noreply.github.com>
2024-07-14 10:00:09 +0700

    Update user.js

--- model/user.js
+++ model/user.js
@@ -6,8 +6,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, default: 'user' },
-    status: { type: String, default: 'active' },
-    token: { type: String}
+    status: { type: String, default: 'active' }
 })
 
 module.exports = mongoose.model('User', userSchema);
